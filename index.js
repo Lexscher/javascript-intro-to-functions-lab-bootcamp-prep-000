bcbc757905147bdf0431c938a2e595bdd31300ec
Alexander Schelchere <ascellie@gmail.com>
2019-04-23 12:20:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,16 +6,25 @@ let logShout = string => console.log(shout(string));
 
 let logWhisper = string => console.log(whisper(string));
 
-// check if a string is uppercase
-let isUpperCase = str => str === str.toUpperCase();
+// // check if a string is uppercase
+// let isUpperCase = str => str === str.toUpperCase();
 
 let sayHiToGrandma = string => {
   let response;
   switch(string) {
     case 'hello':
-      response = 'I can\'t hear you';
+      response = 'I can\'t hear you!';
       break;
     case 'HELLO':
-      response = ''
+      response = 'YES INDEED!';
+      break;
+    case 'I love you, Grandma.':
+      response = 'I love you, too.';
+      break;
+    default:
+      response = 'NOT ONE OF THE EXPECTED INPUTS';
+      break;
   }
+  
+  return response;
 }
