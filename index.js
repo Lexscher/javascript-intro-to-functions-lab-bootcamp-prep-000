010f2b2f9d76b70d766a3bb019b09b4127bd7253
Alexander Schelchere <ascellie@gmail.com>
2019-04-23 12:13:20 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,18 @@
+let shout = string => string.toUpperCase();
+
+let whisper = string => string.toLowerCase();
+
+let logShout = string => console.log(shout(string));
+
+let logWhisper = string => console.log(whisper(string));
+
+let sayHiToGrandma = string => {
+  let response;
+  switch(string.isUpperCase()) {
+    case 'hello':
+      response = 'I can\'t hear you';
+      break;
+    case 'HELLO':
+      response = ''
+  }
+}
